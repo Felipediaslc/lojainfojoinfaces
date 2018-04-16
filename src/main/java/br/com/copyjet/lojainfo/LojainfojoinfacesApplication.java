ade3f1a1e5851330b2153d4c258f134efcae7a06
alex <alex@alex-ubuntu-pc>
2018-04-15 23:28:53 -0300

    Commit inicial do projeto, usando joinfaces.

---
+++ src/main/java/br/com/copyjet/lojainfo/LojainfojoinfacesApplication.java
@@ -0,0 +1,12 @@
+package br.com.copyjet.lojainfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojainfojoinfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojainfojoinfacesApplication.class, args);
+	}
+}
